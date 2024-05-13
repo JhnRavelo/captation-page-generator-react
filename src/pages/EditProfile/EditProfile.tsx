19e1c3bda214a21bde7373da6769106827f49de1
JhnRavelo <johnravelo135@gmail.com>
2024-05-13 16:59:58 +0300

    chore: add profileform to login and edit page

---
+++ src/pages/EditProfile/EditProfile.tsx
@@ -0,0 +1,66 @@
+import EditForm from "../../components/ProfileForm/ProfileForm";
+import { validateProfile } from "../../utils/validationSchema";
+import useAuth from "../../hooks/useAuth";
+
+export type TypeProfileFormFields = {
+  name: "email" | "password" | "confirmPassword" | "name";
+  header: string;
+  placeholder: string;
+  type: string;
+}[];
+
+export type TypeInitialProfile = {
+  email: string;
+  password: string;
+  confirmPassword?: string;
+  name?: string;
+};
+
+const signupFields: TypeProfileFormFields = [
+  {
+    name: "name",
+    header: "Nom d'utilisateur",
+    placeholder: "Nom d'utilisateur",
+    type: "text",
+  },
+  {
+    name: "email",
+    header: "Email",
+    placeholder: "Adresse email",
+    type: "text",
+  },
+  {
+    name: "password",
+    header: "Mot de passe",
+    placeholder: "Mot de passe",
+    type: "password",
+  },
+  {
+    name: "confirmPassword",
+    header: "Confirmez le mot de passe",
+    placeholder: "Confirmez le mot de passe",
+    type: "text",
+  },
+];
+
+const EditProfile = () => {
+  const authContext = useAuth()
+
+  const initialSignupValue: TypeInitialProfile = {
+    email: authContext?.auth?.email ? authContext?.auth?.email : "",
+    password: "",
+    confirmPassword: "",
+    name: authContext?.auth?.name,
+  };
+  return (
+    <EditForm
+      title="Modifier Profile"
+      forms={signupFields}
+      initialValues={initialSignupValue}
+      btnTitle="Envoyer"
+      validate={validateProfile}
+    />
+  );
+};
+
+export default EditProfile;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,21 +1,36 @@
-import "./login.scss";
+import { validateLogin } from "../../utils/validationSchema";
+import LoginForm from "../../components/ProfileForm/ProfileForm";
+import { TypeProfileFormFields } from "../EditProfile/EditProfile";
+
+const initialLoginValues = {
+  email: "",
+  password: "",
+};
+
+const loginFields: TypeProfileFormFields = [
+  {
+    type: "text",
+    name: "email",
+    placeholder: "Adresse Email",
+    header: "Email",
+  },
+  {
+    type: "password",
+    name: "password",
+    placeholder: "Mot de passe",
+    header: "Mot de passe",
+  },
+];
 
 const Login = () => {
   return (
-    <>
-      <div className="background">
-        <div className="shape"></div>
-        <div className="shape"></div>
-      </div>
-      <form>
-        <h3>Connexion</h3>
-        <label htmlFor="username">Email</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
-        <label htmlFor="password">Mot de passe</label>
-        <input type="password" placeholder="Password" id="password" />
-        <button type="submit">Se connecter</button>
-      </form>
-    </>
+    <LoginForm
+      title="Connexion"
+      validate={validateLogin}
+      initialValues={initialLoginValues}
+      forms={loginFields}
+      btnTitle="Se connecter"
+    />
   );
 };
 
