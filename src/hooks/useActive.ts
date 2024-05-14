f016df845abeac68e287d2cab7c6fae98b4efd91
JhnRavelo <johnravelo135@gmail.com>
2024-05-14 08:12:25 +0300

    feat: add class active

---
+++ src/hooks/useActive.ts
@@ -0,0 +1,16 @@
+const useActive = () => {
+  return (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    lists: React.RefObject<HTMLDivElement>
+  ) => {
+    lists.current?.childNodes.forEach((list) => {
+      if (!(list instanceof Element)) return;
+      if (list.classList.contains("active")) {
+        list.classList.remove("active");
+      }
+    });
+    e.currentTarget.classList.add("active");
+  };
+};
+
+export default useActive;
