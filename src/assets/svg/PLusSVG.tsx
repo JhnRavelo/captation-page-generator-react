7f4bb375a014a60fc5f48226a311fdd77968a086
JhnRavelo <johnravelo135@gmail.com>
2024-05-23 10:26:07 +0300

    chore: add addButton component

---
+++ src/assets/svg/PLusSVG.tsx
@@ -0,0 +1,20 @@
+import { SVGPropsType } from "./DashboardSVG";
+
+const PLusSVG = ({ width, height, className }: SVGPropsType) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      className={className}
+      viewBox="0 0 256 256"
+    >
+      <path
+        fill="currentColor"
+        d="M208 28H48a20 20 0 0 0-20 20v160a20 20 0 0 0 20 20h160a20 20 0 0 0 20-20V48a20 20 0 0 0-20-20m-4 176H52V52h152ZM76 128a12 12 0 0 1 12-12h28V88a12 12 0 0 1 24 0v28h28a12 12 0 0 1 0 24h-28v28a12 12 0 0 1-24 0v-28H88a12 12 0 0 1-12-12"
+      />
+    </svg>
+  );
+};
+
+export default PLusSVG;
---
+++ src/components/AddButton/AddButton.tsx
@@ -0,0 +1,13 @@
+import PLusSVG from "../../assets/svg/PLusSVG"
+import "./addButton.scss"
+
+const AddButton = () => {
+  return (
+    <div className="add-button">
+        <span>Nouveau</span>
+        <PLusSVG width="20" height="20" />
+    </div>
+  )
+}
+
+export default AddButton
