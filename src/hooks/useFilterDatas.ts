5af72b92d91bf505b7702aa66752878c1755fa35
JhnRavelo <johnravelo135@gmail.com>
2024-07-26 16:38:27 +0300

    feature: filter qrcode display

--- src/hooks/useFilterDatas.ts
+++ src/hooks/useFilterDatas.ts
@@ -9,18 +9,32 @@ const useFilterDatas = (datas?: Card[]) => {
   const formContext = useForm();
 
   useEffect(() => {
-    if (datas || entrepriseContext?.entreprise || formContext?.year) {
-      const filterCampagnes = datas?.filter(
+    if (datas && entrepriseContext?.entreprise && formContext?.year) {
+      let filterCampagnes = datas?.filter(
         (campagne) =>
           campagne.entreprise == entrepriseContext?.entreprise.company &&
           campagne.dateDebut?.split("-")[0] == formContext?.year
       );
-      
+
+      if (datas[0]?.img && entrepriseContext.media) {
+        filterCampagnes = datas.filter(
+          (campagne) =>
+            campagne.entreprise == entrepriseContext?.entreprise.company &&
+            campagne.dateDebut?.split("-")[0] == formContext?.year &&
+            campagne.media == entrepriseContext.media.media
+        );
+      }
+
       if (filterCampagnes) {
         setFilterDatas(filterCampagnes);
       }
     }
-  }, [entrepriseContext?.entreprise, datas, formContext?.year]);
+  }, [
+    entrepriseContext?.entreprise,
+    datas,
+    formContext?.year,
+    entrepriseContext?.media,
+  ]);
 
   return filterDatas;
 };
--- src/pages/Landing/QRCode/QRCode.tsx
+++ src/pages/Landing/QRCode/QRCode.tsx
@@ -7,10 +7,12 @@ import { qrcodeFields } from "../../../assets/ts/qrcode";
 import { Fragment } from "react";
 import { validateQRCode } from "../../../utils/validationSchema";
 import useQRCode from "../../../hooks/useQRCode";
+import useFilterDatas from "../../../hooks/useFilterDatas";
 
 const QRCode = () => {
   const formContext = useForm();
   const qrCodeContext = useQRCode();
+  const qrCodes = useFilterDatas(qrCodeContext?.qrCodes);
 
   const handleClick = () => {
     formContext?.setOpenForm(true);
@@ -26,9 +28,9 @@ const QRCode = () => {
       <div className="page-cards-container">
         <AddButton handleClick={handleClick} />
         <div className="qr-container cards-container">
-          {qrCodeContext?.qrCodes &&
-            qrCodeContext.qrCodes.length > 0 &&
-            qrCodeContext.qrCodes.map((qrCode, index) => (
+          {qrCodes &&
+            qrCodes.length > 0 &&
+            qrCodes.map((qrCode, index) => (
               <Fragment key={index}>
                 <Card card={qrCode} slug="QR-Code" isClickable={false} />
               </Fragment>
