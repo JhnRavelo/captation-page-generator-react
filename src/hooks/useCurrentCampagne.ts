d7a0c977e3011e4bd676d7d8f568c79bcd0fa8b6
JhnRavelo <johnravelo135@gmail.com>
2024-08-09 16:30:51 +0300

    fix: campagne may be undefined

--- src/hooks/useCurrentCampagne.ts
+++ src/hooks/useCurrentCampagne.ts
@@ -9,26 +9,33 @@ const useCurrentCampagne = () => {
   const campagneContext = useCampagne();
   const campagnes = useFilterDatas(campagneContext?.campagnes);
   const [currentCampagnes, setCurrentCampagnes] = useState<Card[]>([]);
-  const [campagneCheckbox, setCampagneCheckbox] = useState([""]);
+  const [campagneCheckbox, setCampagneCheckbox] = useState<string[]>([]);
   const entrepriseContext = useMediaEntreprise();
 
   useEffect(() => {
-    const listCampagnes = campagnes
-      .map((campagne) => {
-        const currentDate = new Date().getTime();
-        const campagneDate = new Date(campagne.dateFin + " 23:59:59").getTime();
+    if (campagnes && campagnes.length > 0) {
+      const listCampagnes = campagnes
+        .map((campagne) => {
+          const currentDate = new Date().getTime();
+          const campagneDate = new Date(
+            campagne.dateFin + " 23:59:59"
+          ).getTime();
 
-        if (currentDate < campagneDate) {
-          return campagne;
-        }
-      })
-      .filter((campagne) => campagne !== undefined)
-      .filter(
-        (campagne) => campagne.entreprise === entrepriseContext?.entreprise.company
-      );
-    setCurrentCampagnes(listCampagnes);
-    const checkbox = listCampagnes.map((campagne) => campagne.id);
-    setCampagneCheckbox(checkbox);
+          if (
+            currentDate < campagneDate &&
+            campagne.entreprise === entrepriseContext?.entreprise.company
+          ) {
+            return campagne;
+          } else return undefined;
+        })
+        .filter((campagne): campagne is Card => campagne !== undefined);
+      
+      if (listCampagnes.length > 0) {
+        setCurrentCampagnes(listCampagnes);
+        const checkbox = listCampagnes.map((campagne) => campagne.id);
+        setCampagneCheckbox(checkbox);
+      }
+    }
   }, [campagnes]);
 
   return { currentCampagnes, campagneCheckbox };
