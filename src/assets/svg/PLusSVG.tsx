0b6a180a4e5f9cdbbb51498d7b88d6911e21bc85
JhnRavelo <johnravelo135@gmail.com>
2024-08-21 11:53:32 +0300

    feature: add option number

--- src/assets/svg/PLusSVG.tsx
+++ src/assets/svg/PLusSVG.tsx
@@ -1,12 +1,13 @@
 import { SVGPropsType } from "./DashboardSVG";
 
-const PLusSVG = ({ width, height, className }: SVGPropsType) => {
+const PLusSVG = ({ width, height, className, onClick }: SVGPropsType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
       className={className}
+      onClick={onClick}
       viewBox="0 0 256 256"
     >
       <path
--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./options.scss";
 import useActive from "../../hooks/useActive";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import PLusSVG from "../../assets/svg/PLusSVG";
 
 export type TypeOptions = {
   title: string;
@@ -10,18 +11,51 @@ export type TypeOptions = {
 
 type OptionsPropsTypes = {
   options: TypeOptions;
+  type: "standard" | "number";
 };
 
-const Options = ({ options }: OptionsPropsTypes) => {
+const Options = ({ options, type }: OptionsPropsTypes) => {
+  const [stateOptions, setStateOptions] = useState<TypeOptions>(options);
+  const { id } = useParams();
   const optionRef = useRef<HTMLDivElement>(null);
   const active = useActive();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const handleAddOption = () => {
+    const newItem = {
+      title: `${stateOptions.length + 1}`,
+      url:
+        "/marketing/campagne/" +
+        id +
+        "/mail/" +
+        id +
+        "MAIL" +
+        `${stateOptions.length + 1}`,
+    };
+    setStateOptions([...stateOptions, newItem]);
+  };
+
   return (
-    <div className="options-container" ref={optionRef}>
-      {options.length > 0 &&
-        options.map((option, index) => (
+    <div
+      className={
+        type == "number"
+          ? "options-container option-number"
+          : "options-container"
+      }
+      ref={optionRef}
+    >
+      {stateOptions.length > 0 &&
+        stateOptions.map((option, index) => (
           <div
-            className={index == 0 ? "option active" : "option"}
+            className={
+              type == "standard" &&
+              option.url.slice(0, 35) == pathname.slice(0, 35)
+                ? "option active"
+                : type == "number" && option.url == pathname
+                ? "option active"
+                : "option"
+            }
             onClick={(e) => {
               active(e, optionRef);
               navigate(option.url);
@@ -31,6 +65,14 @@ const Options = ({ options }: OptionsPropsTypes) => {
             <span>{option.title}</span>
           </div>
         ))}
+      {type == "number" && stateOptions.length <= 3 ? (
+        <PLusSVG
+          width="40"
+          height="40"
+          className="svg-option-plus"
+          onClick={() => handleAddOption()}
+        />
+      ) : null}
     </div>
   );
 };
