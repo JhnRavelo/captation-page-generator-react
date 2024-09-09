0599259d31aa46f84df3d1abeb878338fe483416
JhnRavelo <johnravelo135@gmail.com>
2024-09-09 15:22:31 +0300

    refactoring: put menus in assets array

---
+++ src/assets/ts/menu.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import DashboardSVG from "../svg/DashboardSVG";
+import MarketingSVG from "../svg/MarketingSVG";
+import QRcodeSVG from "../svg/QRcodeSVG";
+import PageSVG from "../svg/PageSVG";
+import UserSVG from "../svg/UserSVG";
+import NoteBookSVG from "../svg/NoteBookSVG";
+import BellSVG from "../svg/BellSVG";
+import EntrepriseSVG from "../svg/EntrepriseSVG";
+
+export type TypeMenus = {
+  url: string;
+  title: string;
+  SVG: React.ReactNode;
+}[];
+
+export const menus: TypeMenus = [
+  {
+    url: "/marketing",
+    title: "Tableau de bord",
+    SVG: <DashboardSVG height="30" width="30" />,
+  },
+  {
+    url: "/marketing/entreprise",
+    title: "Entreprise",
+    SVG: <EntrepriseSVG height="23" width="30" />,
+  },
+  {
+    url: "/marketing/campagne",
+    title: "Campagne marketing",
+    SVG: <MarketingSVG height="25" width="30" />,
+  },
+  {
+    url: "/marketing/qr-code",
+    title: "Générer un QRCode",
+    SVG: <QRcodeSVG height="20" width="30" />,
+  },
+  {
+    url: "/marketing/page-campagne",
+    title: "Page de captation",
+    SVG: <PageSVG height="24" width="30" />,
+  },
+  {
+    url: "/marketing/user",
+    title: "Utilisateur",
+    SVG: <UserSVG height="20" width="30" />,
+  },
+  {
+    url: "/marketing/log",
+    title: "Journals des événements",
+    SVG: <NoteBookSVG width="30" height="23" />,
+  },
+  {
+    url: "/marketing/notif",
+    title: "Notifications",
+    SVG: <BellSVG height="24" width="30" />,
+  },
+];
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,13 +1,7 @@
 import "./menu.scss";
 import logo from "../../assets/png/Logo_aluhd.png";
 import { Link } from "react-router-dom";
-import DashboardSVG from "../../assets/svg/DashboardSVG";
-import QRcodeSVG from "../../assets/svg/QRcodeSVG";
-import BellSVG from "../../assets/svg/BellSVG";
-import PageSVG from "../../assets/svg/PageSVG";
 import imgAvatar from "../../assets/png/reglages.png";
-import NoteBookSVG from "../../assets/svg/NoteBookSVG";
-import MarketingSVG from "../../assets/svg/MarketingSVG";
 import useActive from "../../hooks/useActive";
 import { useEffect, useRef, useState } from "react";
 import useAuth from "../../hooks/useAuth";
@@ -15,7 +9,7 @@ import useLog from "../../hooks/useLog";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { toast } from "react-toastify";
 import InfoAccount from "../InfoAccount/InfoAccount";
-import UserSVG from "../../assets/svg/UserSVG";
+import { menus } from "../../assets/ts/menu";
 
 const Menu = () => {
   const active = useActive();
@@ -49,53 +43,35 @@ const Menu = () => {
       <div className="separator"></div>
       <div className="navigator">
         <div className="item" ref={menuRef}>
-          <Link to="/marketing" onClick={(e) => active(e, menuRef)}>
-            <DashboardSVG height="30" width="30" />
-            <span>Tableau de bord</span>
-          </Link>
-          <Link to="/marketing/campagne" onClick={(e) => active(e, menuRef)}>
-            <MarketingSVG height="25" width="30" />
-            <span>Campagne marketing</span>
-          </Link>
-          <Link to="/marketing/qr-code" onClick={(e) => active(e, menuRef)}>
-            <QRcodeSVG height="20" width="30" />
-            <span>Générer un QRCode</span>
-          </Link>
-          <Link
-            to="/marketing/page-campagne"
-            onClick={(e) => active(e, menuRef)}
-          >
-            <PageSVG height="24" width="30" />
-            <span>Page de captation</span>
-          </Link>
-          <Link
-            to="/marketing/user"
-            onClick={(e) => active(e, menuRef)}
-          >
-            <UserSVG height="20" width="30" />
-            <span>Utilisateur</span>
-          </Link>
-          <Link to="/marketing/log" onClick={(e) => active(e, menuRef)}>
-            <NoteBookSVG width="30" height="23" />
-            <span>Journals des événements</span>
-          </Link>
-          <Link to="/marketing/notif" onClick={(e) => handleNotification(e)}>
-            <BellSVG
-              height="24"
-              width="30"
-              className={length && length > 0 ? "shake" : ""}
-            />
-            <span>Notifications</span>
-            {length && length > 0 ? (
-              <div className="notif">
-                <span className="span-notif">{length}</span>
-              </div>
-            ) : null}
-          </Link>
+          {menus.map((menu, index) => (
+            <Link
+              to={menu.url}
+              onClick={(e) => {
+                active(e, menuRef);
+                if (menu.title == "Notifications") {
+                  handleNotification(e);
+                }
+              }}
+              key={index}
+              className={
+                menu.title == "Notifications" && length && length > 0
+                  ? "shake"
+                  : ""
+              }
+            >
+              {menu.SVG}
+              <span>{menu.title}</span>
+              {menu.title == "Notifications" && length && length > 0 ? (
+                <div className="notif">
+                  <span className="span-notif">{length}</span>
+                </div>
+              ) : null}
+            </Link>
+          ))}
         </div>
         <InfoAccount
           img={authContext?.auth?.avatar ? authContext.auth.avatar : imgAvatar}
-          name={authContext?.auth?.name ? authContext.auth.name : "Cedric R."}
+          name={authContext?.auth?.name ? authContext.auth.name : "Anonymous"}
         />
       </div>
     </menu>
